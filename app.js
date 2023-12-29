47f89fcc266b30231f8af1a5ea7c0aa0477236e9
SztGellert <sztankgellert@gmail.com>
2023-12-29 14:48:40 +0100

    update workflow and serve necessary resources

--- app.js
+++ app.js
@@ -2,25 +2,28 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 var _ = require("underscore");
+app.use(express.static('./'))
 
 app.get('/', function (req, res) {
     res.send("Node App is running!");
 });
 
-app.get('/api/posts/:id?/:comments?', function (req, res) {
-    var posts = JSON.parse(fs.readFileSync('posts.json', 'utf8'));
 
-    var _id = req.params.id;
+
+app.get('/api/posts/:id?/:comments?', function (req, res) {
+    const posts = JSON.parse(fs.readFileSync('posts.json', 'utf8'));
+
+    const _id = req.params.id;
 
     if (_id && typeof _id !== "undefined") {
-        var filtered = _.where(posts, {id: Number(_id)});
+        const filtered = _.where(posts, {id: Number(_id)});
 
         if (filtered.length === 0) {
             res.status(400).send('Post doesn\'t exist with the given id');
             return;
         }
         if (req.url.indexOf("comments") > -1) {
-            var comments = JSON.parse(fs.readFileSync('comments.json', 'utf8'));
+            const comments = JSON.parse(fs.readFileSync('comments.json', 'utf8'));
             res.send(comments);
             return
         }
@@ -33,19 +36,19 @@ app.get('/api/posts/:id?/:comments?', function (req, res) {
 })
 
 app.get('/api/tags/:name?', function (req, res) {
-    var posts = JSON.parse(fs.readFileSync('posts.json', 'utf8'));
+    const posts = JSON.parse(fs.readFileSync('posts.json', 'utf8'));
 
-    var _name = req.params.name;
+    const _name = req.params.name;
 
     if (_name && typeof _name !== "undefined") {
-        var filtered = posts.filter(post =>
+        const filtered = posts.filter(post =>
             post.tags.filter(t => t.toLowerCase() === _name.toLowerCase()).length > 0
         );
-        res.send(filtered);
+        res.send({"data": filtered});
         return
     }
 
-    res.send(posts);
+    res.send({"data": posts});
 })
 
 app.listen(3000);
---
+++ scripts/index.js
@@ -0,0 +1,4 @@
+let d = new Date();
+let dateDiv = document.createElement('div');
+dateDiv.innerHTML = "<div class='card'>Today's date is " + d + "</div>"
+document.body.appendChild(dateDiv);
