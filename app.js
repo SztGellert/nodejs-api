911e44745ddc5c0d02117be1c5be855aaad2dc03
SztGellert <sztankgellert@gmail.com>
2023-12-29 02:11:00 +0100

    test node app

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+    console.log(req.url, req.method,  req.headers);
+    res.setHeader('Content-Type', 'text/html');
+    res.write('Node App is Running!');
+    res.end();
 });
 
 server.listen(3000);
