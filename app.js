62f532e6e75e51f9ac83622272f898eb03623221
SztGellert <sztankgellert@gmail.com>
2023-12-29 05:16:06 +0100

    setup posts api

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
+
 app.get('/', function (req, res) {
     res.send("Node App is running!");
 });
+
+app.get('/api/posts', function (req, res) {
+    var obj = JSON.parse(fs.readFileSync('posts.json', 'utf8'));
+    res.send(obj);
+})
+
 app.listen(3000);
