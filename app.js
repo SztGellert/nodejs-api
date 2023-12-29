c0d7fb02182dcf508f30f370a0bc8085c7ad16f1
SztGellert <sztankgellert@gmail.com>
2023-12-29 05:44:21 +0100

    added post filtering

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 var express = require('express');
 var app = express();
 var fs = require('fs');
+var _ = require("underscore");
 
 app.get('/', function (req, res) {
     res.send("Node App is running!");
 });
 
-app.get('/api/posts', function (req, res) {
-    var obj = JSON.parse(fs.readFileSync('posts.json', 'utf8'));
+app.get('/api/posts/:id?', function (req, res) {
+    var posts = JSON.parse(fs.readFileSync('posts.json', 'utf8'));
+
+    var _id = req.params.id;
+
+    if (_id && typeof _id !== "undefined") {
+        var filtered = _.where(posts, {id: Number(_id)});
+        res.send(filtered);
+        return
+    }
+
     res.send(obj);
 })
 
