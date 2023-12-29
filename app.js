d750149ff7ce633165dfc47b54da80ab5cef2ccf
SztGellert <sztankgellert@gmail.com>
2023-12-29 14:11:31 +0100

    added checks anf filters to posts api

--- app.js
+++ app.js
@@ -7,18 +7,45 @@ app.get('/', function (req, res) {
     res.send("Node App is running!");
 });
 
-app.get('/api/posts/:id?', function (req, res) {
+app.get('/api/posts/:id?/:comments?', function (req, res) {
     var posts = JSON.parse(fs.readFileSync('posts.json', 'utf8'));
 
     var _id = req.params.id;
 
     if (_id && typeof _id !== "undefined") {
         var filtered = _.where(posts, {id: Number(_id)});
+
+        if (filtered.length === 0) {
+            res.status(400).send('Post doesn\'t exist with the given id');
+            return;
+        }
+        if (req.url.indexOf("comments") > -1) {
+            var comments = JSON.parse(fs.readFileSync('comments.json', 'utf8'));
+            res.send(comments);
+            return
+        }
+
+        res.send({"data": filtered});
+        return
+    }
+
+    res.send({"data": posts});
+})
+
+app.get('/api/tags/:name?', function (req, res) {
+    var posts = JSON.parse(fs.readFileSync('posts.json', 'utf8'));
+
+    var _name = req.params.name;
+
+    if (_name && typeof _name !== "undefined") {
+        var filtered = posts.filter(post =>
+            post.tags.filter(t => t.toLowerCase() === _name.toLowerCase()).length > 0
+        );
         res.send(filtered);
         return
     }
 
-    res.send(obj);
+    res.send(posts);
 })
 
 app.listen(3000);
